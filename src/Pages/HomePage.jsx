61d02effc0b7e2d26c8388fdf090d69b646790b0
Łukasz Szerszyński <lukiszerszen@gmail.com>
2023-12-09 17:42:19 +0100

    HomePage - MobileVersion done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -41,25 +41,32 @@ export default function HomePage() {
       <div className="timeLine">
         <ul>
           <li>
-            <p>In progress1</p>
+            <p>Studia, zarządzanie przedsiębiorstwem turystycznym</p>
+            <p className="data1">2007-2010</p>
           </li>
           <li>
-            <p>In progress2</p>
+            <p>Mechanik rowerowy</p>
+            <p className="data2">2010-teraz</p>
           </li>
           <li>
-            <p>In progress3</p>
+            <p>Rozpoczęcie nauki programowania</p>
+            <p className="data1">kwiecien 2023r</p>
           </li>
           <li>
-            <p>In progress4</p>
+            <p>Ukończenie: Kurs Tworzenia Stron WWW cz.1 Udemy. Pierwsza własna strona.</p>
+            <p className="data2">maj 2023r</p>
           </li>
           <li>
-            <p>In progress5</p>
+            <p>Ukończenie: Kurs Tworzenia Stron WWW cz.2 Udemy. Rozpoczęcie stażu w TDS Soft Alan Urban </p>
+            <p className="data1">czerwiec 2023r</p>
           </li>
           <li>
-            <p>In progress6</p>
+            <p>Ukończenie: Kurs JavaScript. Udemy oraz kurs Tailwind MMC School </p>
+            <p className="data2">sierpień 2023r</p>
           </li>
           <li>
-            <p>In progress7</p>
+            <p>Ukończenie: Kurs Tworzenia Stron WWW cz.3 MMC School. Pierwsza strona w React.</p>
+            <p className="data1">Listopad 2023r</p>
           </li>
         </ul>
       </div>
